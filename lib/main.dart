a51b8c5ceb394c9f368db4794f6517416a79b908
EvandroArmando <evandro.armando@evolium.ao>
2022-07-26 00:23:22 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
